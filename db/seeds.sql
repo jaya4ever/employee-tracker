4c4785740cc0b1bad50bdfaed63ef027b3a6bed0
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-15 11:21:18 -0600

    inserted values into seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,14 @@
 INSERT INTO department (name)
-VALUES ()
+VALUES ("Engineering", "Sales", "Security");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ()
+VALUES ("Software Engineer", 135000.00 , 1),
+("Sale representative", 100000.00 , 2),
+("Security Operator", 140000.00 , 3);
 
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ()
+VALUES ("Carlie", "Simon" , 1, 2),
+("Anna", "Biswas" , 2, 3),
+("Heather", "Chauhan" , 4, 1),
+("Sophiya", "Thom" , 3, 1);
