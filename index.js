458923c8385ecabc6d41b978ca8ee0a450c5e1a5
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-18 10:44:23 -0600

    init the department_db of department, role and employees

--- index.js
+++ index.js
@@ -19,5 +19,16 @@ let dataB;
 }
 async function inquirerUser(){
     await init()
-    
+    const [departments] = await dataB.execute("SELECT department.id, department.name FROM department")
+    const [roles] = await dataB.execute("SELECT role.id, role.title, role.salary FROM role JOIN department ON role.department_id = department.id;" )
+    const [employees] = await db.execute(`SELECT employee.id, employee.first_name, employee.last_name, role.title as roleTitle, department.name as departmentName, role.salary, manager.first_name AS managerName
+    FROM (
+    (employee JOIN role ON role_id = role.id)
+    left join 
+    (SELECT * FROM employee) as manager
+    on manager.id = employee.manager_id
+    JOIN 
+    department 
+    ON department_id = department.id
+    );`)
 }
