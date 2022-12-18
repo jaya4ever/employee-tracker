c28022f5f2798bdc469d99b0f13f1802f2443020
jaya4ever <111536082+jaya4ever@users.noreply.github.com>
2022-12-18 10:23:22 -0600

    creating function to initiation

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 // defining required packages
 const sql = require('mysql2');
 const inquirer = require('inquirer');
+
+let dataB;
+
+ inquirerUser();
+ async function init() {
+    dataB = await mysql.createConnection(
+        {
+            host: '127.0.0.1',
+            user: 'root',
+            password: '7777',
+            database: 'department_db'
+        },
+        console.log(`Connected to the department_db database.`)
+    );
+}
